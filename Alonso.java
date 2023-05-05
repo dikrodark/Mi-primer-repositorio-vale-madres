47da71ea1936d8037e56fbca74f6695618a39b06
Diego <diegoesteban.barreracopca@gmail.com>
2023-05-05 09:14:35 -0600

    El pinche primer commit del Dieco

---
+++ Alonso.java
@@ -0,0 +1,10 @@
+
+public class Alonso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esta es la primera version del programa Alonso Huevon");
+		System.out.println("Alonso ya falto 1 DIA");
+	}
+
+}
